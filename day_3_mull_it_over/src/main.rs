dbe5a85e734f6262a7697b922335703a8d2ca280
vladislav-karamfilov <vladislav.karamfilov@gmail.com>
2024-12-03 11:03:46 +0200

    fix day 3 puzzle 2 solution (how input is read is important!)

--- day_3_mull_it_over/src/main.rs
+++ day_3_mull_it_over/src/main.rs
@@ -9,45 +9,34 @@ fn main() {
 fn solve_puzzle2() {
     let mut result = 0;
 
+    let program_memory = read_program_memory();
+
+    let mut multiplication_enabled = true;
+    let mut text_to_process = program_memory.as_str();
     loop {
-        let mut line = String::new();
+        if multiplication_enabled {
+            let disable_start_index = text_to_process.find("don't()");
+            match disable_start_index {
+                Some(start_index) => {
+                    result += calculate_sum_of_multiplications(&text_to_process[..start_index]);
 
-        std::io::stdin()
-            .read_line(&mut line)
-            .expect("Failed to read line");
-
-        let trimmed_line = line.trim();
-        if trimmed_line.is_empty() {
-            break;
-        }
-
-        let mut multiplication_enabled = true;
-        let mut text_to_process = trimmed_line;
-        loop {
-            if multiplication_enabled {
-                let disable_start_index = text_to_process.find("don't()");
-                match disable_start_index {
-                    Some(start_index) => {
-                        result += calculate_sum_of_multiplications(&text_to_process[..start_index]);
-
-                        text_to_process = &text_to_process[(start_index + 1)..];
-                        multiplication_enabled = false;
-                    }
-                    None => {
-                        result += calculate_sum_of_multiplications(text_to_process);
-
-                        break;
-                    }
+                    text_to_process = &text_to_process[start_index..];
+                    multiplication_enabled = false;
                 }
-            } else {
-                let enable_start_index = text_to_process.find("do()");
-                match enable_start_index {
-                    Some(start_index) => {
-                        text_to_process = &text_to_process[(start_index + 1)..];
-                        multiplication_enabled = true;
-                    }
-                    None => break,
+                None => {
+                    result += calculate_sum_of_multiplications(text_to_process);
+
+                    break;
                 }
+            }
+        } else {
+            let enable_start_index = text_to_process.find("do()");
+            match enable_start_index {
+                Some(start_index) => {
+                    text_to_process = &text_to_process[start_index..];
+                    multiplication_enabled = true;
+                }
+                None => break,
             }
         }
     }
@@ -57,7 +46,15 @@ fn solve_puzzle2() {
 
 #[allow(dead_code)]
 fn solve_puzzle1() {
-    let mut result = 0;
+    let program_memory = read_program_memory();
+
+    let result = calculate_sum_of_multiplications(program_memory.as_str());
+
+    println!("{result}");
+}
+
+fn read_program_memory() -> String {
+    let mut result = String::new();
 
     loop {
         let mut line = String::new();
@@ -71,10 +68,14 @@ fn solve_puzzle1() {
             break;
         }
 
-        result += calculate_sum_of_multiplications(trimmed_line);
+        if !result.is_empty() {
+            result.push(' ');
+        }
+
+        result.push_str(trimmed_line);
     }
 
-    println!("{result}");
+    result
 }
 
 fn calculate_sum_of_multiplications(text_to_process: &str) -> u32 {
@@ -85,15 +86,15 @@ fn calculate_sum_of_multiplications(text_to_process: &str) -> u32 {
 
     let multiplication_pattern = Regex::new(r"mul\((\d+),(\d+)\)").unwrap();
 
-    for captures in multiplication_pattern.captures_iter(text_to_process) {
-        let x = captures
+    for curr_match in multiplication_pattern.captures_iter(text_to_process) {
+        let x = curr_match
             .get(1)
             .map(|m| m.as_str())
             .unwrap()
             .parse::<u32>()
             .unwrap();
 
-        let y = captures
+        let y = curr_match
             .get(2)
             .map(|m| m.as_str())
             .unwrap()
