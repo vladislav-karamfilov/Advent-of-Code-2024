39bae47a4eddd88328ad34cc3b3128b81ecb35b5
vladislav-karamfilov <vladislav.karamfilov@gmail.com>
2024-12-04 09:51:29 +0200

    solve puzzle 2 day 4

--- day_4_ceres_search/src/main.rs
+++ day_4_ceres_search/src/main.rs
@@ -1,10 +1,25 @@
 fn main() {
-    solve_puzzle1();
-    // solve_puzzle2();
+    // solve_puzzle1();
+    solve_puzzle2();
 }
 
 #[allow(dead_code)]
-fn solve_puzzle2() {}
+fn solve_puzzle2() {
+    let word_search = read_word_search();
+
+    let max_col = word_search[0].len() - 1;
+
+    let mut result = 0;
+    for row in 0..word_search.len() {
+        for col in 0..=max_col {
+            if is_x_mas_word(row, col, &word_search) {
+                result += 1;
+            }
+        }
+    }
+
+    println!("{result}");
+}
 
 #[allow(dead_code)]
 fn solve_puzzle1() {
@@ -31,25 +46,46 @@ fn solve_puzzle1() {
                 result += 1;
             }
 
-            if is_xmas_word(row, col, Direction::UpAndRight, &word_search) {
+            if is_xmas_word(row, col, Direction::UpRight, &word_search) {
                 result += 1;
             }
 
-            if is_xmas_word(row, col, Direction::UpAndLeft, &word_search) {
+            if is_xmas_word(row, col, Direction::UpLeft, &word_search) {
                 result += 1;
             }
 
-            if is_xmas_word(row, col, Direction::DownAndRight, &word_search) {
+            if is_xmas_word(row, col, Direction::DownRight, &word_search) {
                 result += 1;
             }
 
-            if is_xmas_word(row, col, Direction::DownAndLeft, &word_search) {
+            if is_xmas_word(row, col, Direction::DownLeft, &word_search) {
                 result += 1;
             }
         }
     }
 
     println!("{result}");
+}
+
+fn is_x_mas_word(start_row: usize, start_col: usize, word_search: &[Vec<char>]) -> bool {
+    if word_search[start_row][start_col] != 'A' {
+        return false; // A must be in the center of the X-MAS
+    }
+
+    let max_row = word_search[0].len() - 1;
+    let max_col = word_search.len() - 1;
+
+    if start_row == 0 || start_row == max_row || start_col == 0 || start_col == max_col {
+        return false; // Some of the neighbor cells would be outside of the boundaries
+    }
+
+    let up_left = word_search[start_row - 1][start_col - 1];
+    let up_right = word_search[start_row - 1][start_col + 1];
+    let down_left = word_search[start_row + 1][start_col - 1];
+    let down_right = word_search[start_row + 1][start_col + 1];
+
+    ((up_left == 'M' && down_right == 'S') || (up_left == 'S' && down_right == 'M'))
+        && ((up_right == 'M' && down_left == 'S') || (up_right == 'S' && down_left == 'M'))
 }
 
 fn is_xmas_word(
@@ -90,7 +126,7 @@ fn is_xmas_word(
                 && word_search[start_row + 2][start_col] == 'A'
                 && word_search[start_row + 3][start_col] == 'S'
         }
-        Direction::UpAndRight => {
+        Direction::UpRight => {
             start_row >= 3
                 && start_col + 3 <= max_col
                 && word_search[start_row][start_col] == 'X'
@@ -98,7 +134,7 @@ fn is_xmas_word(
                 && word_search[start_row - 2][start_col + 2] == 'A'
                 && word_search[start_row - 3][start_col + 3] == 'S'
         }
-        Direction::UpAndLeft => {
+        Direction::UpLeft => {
             start_row >= 3
                 && start_col >= 3
                 && word_search[start_row][start_col] == 'X'
@@ -106,7 +142,7 @@ fn is_xmas_word(
                 && word_search[start_row - 2][start_col - 2] == 'A'
                 && word_search[start_row - 3][start_col - 3] == 'S'
         }
-        Direction::DownAndRight => {
+        Direction::DownRight => {
             start_row + 3 <= max_row
                 && start_col + 3 <= max_col
                 && word_search[start_row][start_col] == 'X'
@@ -114,7 +150,7 @@ fn is_xmas_word(
                 && word_search[start_row + 2][start_col + 2] == 'A'
                 && word_search[start_row + 3][start_col + 3] == 'S'
         }
-        Direction::DownAndLeft => {
+        Direction::DownLeft => {
             start_row + 3 <= max_row
                 && start_col >= 3
                 && word_search[start_row][start_col] == 'X'
@@ -151,8 +187,8 @@ enum Direction {
     Left,
     Up,
     Down,
-    UpAndRight,
-    UpAndLeft,
-    DownAndRight,
-    DownAndLeft,
+    UpRight,
+    UpLeft,
+    DownRight,
+    DownLeft,
 }
