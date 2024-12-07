2fddf942891b7b1a352d049b391d50a6da18e5a6
vladislav-karamfilov <vladislav.karamfilov@gmail.com>
2024-12-07 10:04:51 +0200

    solve puzzle 2 day 7

--- day_7_bridge_repair/src/main.rs
+++ day_7_bridge_repair/src/main.rs
@@ -1,5 +1,19 @@
 fn main() {
-    solve_puzzle1();
+    // solve_puzzle1();
+    solve_puzzle2();
+}
+
+#[allow(dead_code)]
+fn solve_puzzle2() {
+    let equations = read_equations();
+
+    let result = equations
+        .iter()
+        .filter(|e| is_equation_possible_with_concatenation_operator(e))
+        .map(|e| e.test_value)
+        .sum::<u64>();
+
+    println!("{result}");
 }
 
 #[allow(dead_code)]
@@ -8,14 +22,40 @@ fn solve_puzzle1() {
 
     let result = equations
         .iter()
-        .filter(|e| is_equation_possible(e))
+        .filter(|e| is_equation_possible_without_concatenation_operator(e))
         .map(|e| e.test_value)
         .sum::<u64>();
 
     println!("{result}");
 }
 
-fn is_equation_possible(equation: &Equation) -> bool {
+fn is_equation_possible_with_concatenation_operator(equation: &Equation) -> bool {
+    let operators_count = equation.operands.len() - 1;
+    let operator_combinations_count = 3_i32.pow(operators_count as u32);
+    let mut operators = vec![Operator::Add; operators_count];
+
+    for i in 0..operator_combinations_count {
+        let mut value = i;
+
+        for i in 0..operators_count {
+            match value % 3 {
+                0 => operators[i] = Operator::Add,
+                1 => operators[i] = Operator::Multiply,
+                _ => operators[i] = Operator::Concatenate,
+            }
+
+            value /= 3;
+        }
+
+        if is_equation_true(equation, &operators) {
+            return true;
+        }
+    }
+
+    false
+}
+
+fn is_equation_possible_without_concatenation_operator(equation: &Equation) -> bool {
     let operators_count = equation.operands.len() - 1;
     let operator_combinations_count = 1 << operators_count;
     let mut operators = vec![Operator::Add; operators_count];
@@ -44,6 +84,12 @@ fn is_equation_true(equation: &Equation, operators: &[Operator]) -> bool {
         match operator {
             Operator::Add => current_result += equation.operands[i + 1],
             Operator::Multiply => current_result *= equation.operands[i + 1],
+            Operator::Concatenate => {
+                let mut new_result = current_result.to_string();
+                new_result.push_str(&equation.operands[i + 1].to_string());
+
+                current_result = new_result.parse().unwrap();
+            }
         }
 
         if current_result > equation.test_value {
@@ -93,6 +139,7 @@ fn read_equations() -> Vec<Equation> {
 enum Operator {
     Add,
     Multiply,
+    Concatenate,
 }
 
 struct Equation {
